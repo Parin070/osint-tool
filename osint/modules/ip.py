c2084cfbe8f09e2d9c939ab35ea0fcca1df308c1
Parin727 <parinarora2007@gmail.com>
2026-06-19 21:56:06 +0530

    Phase 2: Virustotal, shodan and abuseipdb integration

--- osint/modules/ip.py
+++ osint/modules/ip.py
@@ -1,4 +1,5 @@
 from .base import Recon
+from shodan import Shodan
 import os
 import requests
 import json
@@ -6,17 +7,35 @@ import json
 class IPRecon(Recon):
 
     def run(self):
-        url = 'https://api.abuseipdb.com/api/v2/check'
+        #AbuseIPDB
+        url_ab = 'https://api.abuseipdb.com/api/v2/check'
 
         querystring = {
             'ipAddress': self.target,
             'maxAgeInDays': '90'
         }
 
-        headers = {
+        headers_ab = {
             'Accept': 'application/json',
             'Key': os.getenv("ABUSEIPDB")
         }
+        abuse_response = requests.get(url_ab, headers=headers_ab, params=querystring).json()
 
-        response = requests.get(url, headers=headers, params=querystring)
-        self.results = response.json()
+        #Shodan
+        api = Shodan(os.getenv("SHODAN"))
+        shodan_response = api.host(self.target)
+        
+        #VirusTotal
+        url_vt = f"https://www.virustotal.com/api/v3/ip_addresses/{self.target}"
+        headers_vt = {
+            "accept": "application/json",
+            "x-apikey": os.getenv("VIRUSTOTAL")
+        }
+
+        vt_response = requests.get(url_vt, headers=headers_vt).text
+
+        self.results = {
+            "AbuseIPDB": abuse_response,
+            "Shodan": shodan_response,
+            "VirusTotal": vt_response
+        }
