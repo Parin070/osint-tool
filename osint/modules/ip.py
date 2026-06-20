95c024b15f4970aa52b76e79f27d30431fd9e159
Parin727 <parinarora2007@gmail.com>
2026-06-20 10:39:13 +0530

    phase2: ip module complete with risk score

--- osint/modules/ip.py
+++ osint/modules/ip.py
@@ -32,7 +32,7 @@ class IPRecon(Recon):
             "x-apikey": os.getenv("VIRUSTOTAL")
         }
 
-        vt_response = requests.get(url_vt, headers=headers_vt).text
+        vt_response = requests.get(url_vt, headers=headers_vt).json()
 
         self.results = {
             "AbuseIPDB": abuse_response,
@@ -43,10 +43,10 @@ class IPRecon(Recon):
     
     def calculate_risk(self):
         abuse_score = self.results["AbuseIPDB"]["data"]["abuseConfidenceScore"]
-        vt_stats = self.results["VirusTotal"]["data"]["attributes"]["last_analysis_stats"]
-        #error in vt stats 
+        vt_stats = self.results["VirusTotal"]["data"]["attributes"]["last_analysis_stats"] 
         vt_score = (vt_stats["malicious"]/sum(vt_stats.values())) * 100
         shodan_score = 100 if self.results["Shodan"].get("vulns") else 0
 
         risk_score = (abuse_score + vt_score + shodan_score)/3
         self.results["risk_score"] = round(risk_score, 2)
+        
